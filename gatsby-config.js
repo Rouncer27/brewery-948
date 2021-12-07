de0eefd952a6b79a18f4fc22b8b59541a7b273d4
Rouncer27 <trevor@switchbackcreative.ca>
2021-12-07 10:47:56 -0700

    GA number

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: [""],
+        trackingIds: ["G-FTMLC1PJJQ"],
       },
     },
     {
